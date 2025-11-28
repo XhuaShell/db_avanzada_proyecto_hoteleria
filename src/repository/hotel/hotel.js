a3104eb8c99c6477870abc97cff59e6b28468af0
Juan-Bernal <user.stranger88@gmail.com>
2025-11-28 17:43:53 -0500

    Update hotel.js

--- src/repository/hotel/hotel.js
+++ src/repository/hotel/hotel.js
@@ -1,1 +1,25 @@
+import { Hotel } from "../model/hotel.";
+
+obtenerHoteles() {
+    SELECT * FROM Hotel;
+}
+
+encontrarHotelPorId(id_hotel) {
+    SELECT * FROM Hotel WHERE id_hotel = ?;
+}
+
+guardarHotel(nombre, direccion, anio_inauguracion, id_categoria) {
+    INSERT INTO Hotel (nombre, direccion, anio_inauguracion, id_categoria)
+    VALUES (?, ?, ?, ?);
+}
+
+actualizarHotel(id_hotel, nombre, direccion, anio_inauguracion, id_categoria) {
+    UPDATE Hotel
+    SET nombre = ?, direccion = ?, anio_inauguracion = ?, id_categoria = ?
+    WHERE id_hotel = ?;
+}
+
+removerHotel(id_hotel) {
+    DELETE FROM Hotel WHERE id_hotel = ?;
+}
 
